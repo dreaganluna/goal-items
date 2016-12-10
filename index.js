6d06adb5a235c6ef524b04c627661367ee903cca
Mikaël Spruyt <draeganlunacrivassan@gmail.com>
2016-12-10 16:38:33 +0100

    add Restify package, read config file

--- index.js
+++ index.js
@@ -5,7 +5,17 @@
 
 var init = function()
 {
-	Winston.verbose('hello');
+	Config.getFromFile("config.json", function(err, config)
+	{
+		if(err)
+		{
+			Winston.error(err.message);
+			process.exit();
+		}
+
+		_config = config;
+		Winston.info('Started with the following config', _config);
+	});
 };
 
 
@@ -14,6 +24,9 @@ var init = function()
 // ================== //
 
 
+var _config;
+var Config  = require("./lib/config.js");
+var Restify = require('restify');
 var Winston = require('./lib/log.js')("goal-items");
 
 init();
