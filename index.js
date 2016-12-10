ff2a327ce4e6fb48cd8d4acfa19ee4a26cc2c81f
Mikaël Spruyt <draeganlunacrivassan@gmail.com>
2016-12-10 16:40:25 +0100

    init restify service

--- index.js
+++ index.js
@@ -15,8 +15,47 @@ var init = function()
 
 		_config = config;
 		Winston.info('Started with the following config', _config);
+
+		initRestify();
 	});
 };
+
+
+var initRestify = function()
+{
+	// create server
+	var server = Restify.createServer({'name': 'goal-items'});
+	server.use(Restify.fullResponse());
+	server.use(Restify.bodyParser());
+	server.use(Restify.queryParser());
+
+	server.on("uncaughtException", onUncaughtException);
+	server.use(mainHandler);
+
+	server.listen(_config.port, serverUpHandler);
+};
+
+
+var mainHandler = function(request, result, next)
+{
+	Winston.log('info', 'Incoming call: ' + request.method + ' ' + request.url);
+
+	next();
+};
+
+
+var onUncaughtException = function(request, response, route, err)
+{
+	Winston.error("Uncaught Exception:\n", err);
+	response.send(err); // Resume default behaviour.
+}
+
+
+var serverUpHandler = function()
+{
+	Winston.log('info', 'Restify server up and running on port ' + _config.port);
+};
+
 
 
 // ================== //
